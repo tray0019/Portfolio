e456ff1f0c14612459f9897b89e932b78aa23961
tray0019 <tray0019@algonquinlive.com>
2026-07-15 18:40:30 -0400

    Fix navbar err

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
-<nav className="navbar">
-  <div className="navbar-contact">
-    <span>Rustom Trayvilla</span>
-    <a href="mailto:rtrayv@gmail.com">Email</a>
-  </div>
+import "./NavBar.css";
 
-  <div className="navbar-links">
-    <a href="#home">Home</a>
-    <a href="#projects">Projects</a>
-    <a href="#experience">Experience</a>
-    <a href="#tech-stacks">Tech Stacks</a>
-  </div>
-</nav>
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-links">
+        <a href="#home">Home</a>
+        <a href="#projects">Projects</a>
+        <a href="#experience">Experience</a>
+        <a href="#tech-stacks">Tech Stacks</a>
+      </div>
+
+      <div className="navbar-contact">
+        <span>Rustom Trayvilla</span>
+        <a href="mailto:rtrayv@gmail.com">Email</a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
